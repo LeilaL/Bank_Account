713c1af5b84ac8536fa311b2f934fdea01e0f5d6
Leila.L <leila.lababsa@hotmail.fr>
2017-11-06 11:59:24 +0100

    changed html (title,buttons,text)

--- views/formView.php
+++ views/formView.php
@@ -2,40 +2,22 @@
 include('template/header.php');
 ?>
 
-  <h5 class="header center-align">Formulaire Ajout Véhicule</h5>
+  <h5 class="header center-align">Formulaire Ajout Compte</h5>
   <a href="index.php" id="return"><strong>Retour</strong></a>
   <div id="form" class="container">
   <div class="row">
 
     <form class="col s12" action="index.php" method="post">
 
-      <!-- BUTTONS RADIO -->
-      <p>Type de véhicule</p>
-<p>
-           <input class="with-gap" name="type" type="radio" value="car" id="test1" />
-           <label for="test1">Voiture</label>
-
-           <input class="with-gap" name="type" type="radio" value="truck" id="test2" />
-           <label for="test2">Camion</label>
-
-           <input class="with-gap" name="type" type="radio" value="bike" id="test3"  />
-           <label for="test3">Moto</label>
-         </p>
-         <!-- END OF BUTTONS RADIO-->
 
       <div class="input-field col s12 m10 l7">
-        <input id="text1" name="brand" type="text" class="validate">
-        <label for="text1">Marque</label>
+        <input id="text1" name="name" type="text" class="validate">
+        <label for="text1">Nom</label>
       </div>
 
       <div class="input-field col s12 m10 l7">
-        <input id="year" name="year" type="text" class="validate">
-        <label for="year">Année</label>
-      </div>
-
-      <div class="input-field col s12 m10 l7">
-        <input id="text2" name="color" type="text" class="validate">
-        <label for="text2">Couleur</label>
+        <input id="year" name="amount" type="text" class="validate">
+        <label for="year">Montant</label>
       </div>
 
       <div class="input-field col s10">
--- views/indexView.php
+++ views/indexView.php
@@ -6,21 +6,21 @@
 <!-- VEHICULE CARD -->
    <div class="container-fluid">
      <div class="row">
-       <a id="button" class="waves-effect btn-large teal lighten-3" href="form.php">Ajouter un véhicule</a></p>
+       <a id="button" class="waves-effect btn-large teal lighten-3" href="form.php">Ajouter un compte</a></p>
        <?php foreach ($show_vehicles as $infosvehicle) {
          ?>
          <div class="col s12 m4 l4">
            <div class="card teal darken-4">
              <div class="card-content white-text">
-               <span class="card-title"><strong>Véhicule</strong></span>
+               <span class="card-title"><strong>Compte N°</strong></span>
                <span class="card-title"><?php echo $infosvehicle->getType()?></span>
                <p><?php echo $infosvehicle->getBrand()?></p>
                <p><?php echo $infosvehicle->getYear()?></p>
                <p><?php echo $infosvehicle->getColor()?></p>
              </div>
              <div class="card-action">
-               <a class="link" href="single.php?join=<?php echo $infosvehicle->getId()?>">Voir</a>
-               <a class="link" href="#modal<?php echo $infosvehicle->getId();?>">Éditer</a>
+               <!-- <a class="link" href="single.php?join=<?php echo $infosvehicle->getId()?>">Voir</a> -->
+               <a class="link" href="#modal<?php echo $infosvehicle->getId();?>">Effectuer des transactions</a>
                <a class="link" href="index.php?delete=<?php echo $value['id'];?>">Supprimer</a>
              </div>
            </div>
@@ -33,12 +33,12 @@
 
 <!-- ==================================== -->
 <!-- Modal Form EDIT -->
-<div id="modal<?php echo $infosvehicle->getId();?>" class="modal">
+<!-- <div id="modal<?php echo $infosvehicle->getId();?>" class="modal">
   <div class="modal-content">
-  <form class="col s12" action="index.php" method="post">
+  <form class="col s12" action="index.php" method="post"> -->
 
     <!-- BUTTONS RADIO -->
-    <p>Type de véhicule</p>
+    <!-- <p>Type de véhicule</p>
      <p>
          <input class="with-gap" name="type" type="radio" value="<?php echo $infosvehicle->getType()?>" id="test1" />
          <label for="test1">Voiture</label>
@@ -73,7 +73,7 @@
 
   </form>
   </div>
-</div>
+</div> -->
 <!-- End Form EDIT -->
 
 
